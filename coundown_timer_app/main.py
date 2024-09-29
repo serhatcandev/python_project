9806f40c35234ea3f72cb3b4ec4427805974dfb8
Serhat Can <85739464+serhatcandev@users.noreply.github.com>
2024-09-30 00:40:43 +0300

    Update main.py

--- coundown_timer_app/main.py
+++ coundown_timer_app/main.py
@@ -1,3 +1,7 @@
+# Exercise 1
+
+### Countdown Timer With ChatGPT
+
 import time
 
 my_time = int(input("Enter the time in seconds: "))
@@ -9,3 +13,20 @@ while my_time > 0:
     time.sleep(1)
 
 print("Countdown completed!")
+
+# Exercise 2
+
+### Countdown Timer With Bro Code
+
+import time
+
+my_time = int(input("Enter the time in seconds: "))
+
+for x in range(my_time,0, -1):
+    seconds = x % 60
+    minitues = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minitues:02}:{seconds:02}")
+    time.sleep(1)
+
+print("Time is UP.")
